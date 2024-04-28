79b6b3248ef52f03a678d77458a23d4e46b76653
mushfiqueyeasir <mushfiqueyeasir@gmail.com>
2024-04-29 00:31:43 +0600

    fix

--- components/Preview/Preview.tsx
+++ components/Preview/Preview.tsx
@@ -16,25 +16,29 @@ const Preview = ({ body }) => {
     };
 
     const handleZoom = (e) => {
-        // Invert zoom factor for scrolling up and down
         const zoomFactor = e.deltaY > 0 ? -0.1 : 0.1;
-        const newZoomLevel = Math.max(0.1, Math.min(zoomLevel + zoomFactor, 3)); // Limit zoom level between 0.1 and 3
+        const newZoomLevel = Math.max(0.1, Math.min(zoomLevel + zoomFactor, 3));
         setZoomLevel(newZoomLevel);
     };
 
     return (
-        <div className="relative overflow-x-auto overflow-y-auto py-6" onWheel={handleZoom}>
-            <div style={{ transform: `scale(${zoomLevel})` }}>
-                <PreviewComponent htmlString={body.code} />
-            </div>
-            <div className="absolute right-4 top-4 flex flex-col gap-y-4">
-                <button onClick={zoomIn} className="rounded-full bg-gray-400 p-3">
-                    <FaPlus />
-                </button>
-                <button onClick={zoomOut} className="rounded-full bg-gray-400 p-3">
-                    <FaMinus />
-                </button>
-            </div>
+        <div className="relative py-6" onWheel={handleZoom}>
+            {body && (
+                <>
+                    {" "}
+                    <div className=" overflow-x-auto overflow-y-auto" style={{ transform: `scale(${zoomLevel})` }}>
+                        <PreviewComponent htmlString={body.code} />
+                    </div>
+                    <div className="absolute right-4 top-4 flex flex-col gap-y-4">
+                        <button onClick={zoomIn} className="rounded-full bg-gray-400 p-3">
+                            <FaPlus />
+                        </button>
+                        <button onClick={zoomOut} className="rounded-full bg-gray-400 p-3">
+                            <FaMinus />
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
