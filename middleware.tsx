71d3c33d1f2bbf24a6b8598e9b5ad62becd6ca47
mushfiqueyeasir <mushfiqueyeasir@gmail.com>
2024-04-28 18:36:14 +0600

    fix

--- middleware.tsx
+++ middleware.tsx
@@ -3,18 +3,15 @@ import type { NextRequest } from "next/server";
 
 export const middleware = (request: NextRequest) => {
     let user = request.cookies.get("user");
-
-    return NextResponse.next()
-    
-    // if (user) {
-    //     if (request.nextUrl.pathname.startsWith("/sign-in")) {
-    //         return NextResponse.redirect(new URL("/", request.url));
-    //     } else {
-    //         return NextResponse.next();
-    //     }
-    // } else {
-    //     return NextResponse.redirect(new URL("/sign-in", request.url));
-    // }
+    if (user) {
+        if (request.nextUrl.pathname.startsWith("/sign-in")) {
+            return NextResponse.redirect(new URL("/", request.url));
+        } else {
+            return NextResponse.next();
+        }
+    } else {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
 
 };
 
