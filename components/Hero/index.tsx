796a8c42e0ea9b359d4a9adccef07e88697f432b
mushfiqueyeasir <mushfiqueyeasir@gmail.com>
2024-04-29 00:36:27 +0600

    fix

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
 "use client";
-import {  useState } from "react";
+import { useEffect, useState } from "react";
 import { imageUrl } from "@/content/imageSliderData";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
@@ -16,12 +16,26 @@ const Hero = () => {
     const prevImage = () => {
         setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? imageUrl.length - 1 : prevIndex - 1));
     };
+
     const handleMouseDown = (event) => {
         setStartX(event.clientX);
     };
 
+    const handleTouchStart = (event) => {
+        setStartX(event.touches[0].clientX);
+    };
+
     const handleMouseUp = (event) => {
         const endX = event.clientX;
+        handleSwipe(endX);
+    };
+
+    const handleTouchEnd = (event) => {
+        const endX = event.changedTouches[0].clientX;
+        handleSwipe(endX);
+    };
+
+    const handleSwipe = (endX) => {
         const diff = endX - startX;
 
         if (diff > 50) {
@@ -30,11 +44,23 @@ const Hero = () => {
             nextImage();
         }
     };
+    useEffect(() => {
+        const timer = setInterval(() => {
+            nextImage();
+        }, 3000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, [currentImageIndex]);
+
     return (
         <section
             className="relative !my-0 h-[90vh] overflow-hidden !py-0 md:h-[93vh]"
             onMouseDown={handleMouseDown}
             onMouseUp={handleMouseUp}
+            onTouchStart={handleTouchStart}
+            onTouchEnd={handleTouchEnd}
         >
             <div className="relative">
                 <div className="hidden lg:block">
@@ -60,7 +86,7 @@ const Hero = () => {
             <div className="absolute bottom-4 z-[2] flex w-full items-center justify-center gap-1 sm:gap-2 md:bottom-5 lg:bottom-[40px]">
                 {imageUrl.map((item: string, index: number) => (
                     <button
-                    onClick={()=>setCurrentImageIndex(index)}
+                        onClick={() => setCurrentImageIndex(index)}
                         key={index}
                         className={`h-1 w-5 rounded-[5px] ${
                             currentImageIndex === index ? "bg-primary" : "bg-white"
